97a9551f65be86f3007d9bac7dd36acb88594740
Nicolas Dao <nicolas.dao@gmail.com>
2018-07-04 22:38:29 +1000

    chore: Add unit test + better eslint options

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const { assert } = require('chai')
+
+describe('main', () => 
+	describe('#do something', () => {
+		it('Should do something well.', () => {
+			assert.equal('1', '1')
+		})
+		it('Should do something else well.', () => {
+			assert.equal('2', '2')
+		})}))
